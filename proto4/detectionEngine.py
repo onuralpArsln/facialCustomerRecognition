1220b27b335c9dad17d6f92f04da5c59c2dd75f3
hackerFaruk <onuralparslan97@gmail.com>
2024-11-21 19:29:41 +0300

    added methods with good system

--- proto4/detectionEngine.py
+++ proto4/detectionEngine.py
@@ -41,6 +41,32 @@ class DetectionEngine:
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
+    def getBodies(self,image,show=False,method=1):
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                # Load the Haar cascade for face detection
+
+        match method:
+            case 0:
+                body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+                bodies = body_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=3, minSize=(50, 100))
+            case 1:
+                hog = cv2.HOGDescriptor()
+                hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+                bodies, _ = hog.detectMultiScale(image, winStride=(8, 8), padding=(8, 8), scale=1.05)
+        
+        
+        
+        print(f"Number of bodies detected: {len(bodies)}")
+
+        # Display individual faces
+        if show:
+            for i, (x, y, w, h) in enumerate(bodies):
+                    body = image[y:y+h, x:x+w]  
+                    window_name = f"body {i+1}"
+                    cv2.imshow(window_name, body)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 # Main program
@@ -54,3 +80,8 @@ if __name__ == "__main__":
 
     testEngine.getFaces(image, show=True)
 
+
+    image_path = "body.png"
+
+    image = cv2.imread(image_path)
+    testEngine.getBodies(image, show=True,method=1)
