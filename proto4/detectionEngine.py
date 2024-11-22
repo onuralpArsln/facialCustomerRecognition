fb95d9014505206c3a65b9c910a71df621010021
hackerFaruk <onuralparslan97@gmail.com>
2024-11-22 16:49:59 +0300

    performance improvements

--- proto4/detectionEngine.py
+++ proto4/detectionEngine.py
@@ -3,37 +3,30 @@ import os
 
 
 class DetectionEngine:
+
+    def __init__(self,) -> None:
+        ###generate Face detectors ,
+        #1
+        self.frontalFaceHaarCascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+        ### generate body detectors
+        #1
+        self.body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+        #2
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     
     def detectFaceLocations(self,image,show=False):
-        # Load the Haar cascade for face detection
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-        # Convert the image to grayscale (Haar cascades work on grayscale images)
+        #haar cascade verimliliği için gray scale al
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the image
-        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        # haarcascade kullan
+        faces = self.frontalFaceHaarCascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
         if show:
             for (x, y, w, h) in faces:
                     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Blue rectangle
             cv2.imshow("Original Image with Faces", image)
-
-        return faces
-    
-    def getFaces(self,image,show=False):
-                # Load the Haar cascade for face detection
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-        # Convert the image to grayscale (Haar cascades work on grayscale images)
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the image
-        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        print(f"Number of faces detected: {len(faces)}")
-
-        # Display individual faces
-        if show:
             for i, (x, y, w, h) in enumerate(faces):
                     face = image[y:y+h, x:x+w]  
                     window_name = f"Face {i+1}"
@@ -41,25 +34,19 @@ class DetectionEngine:
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-    def getBodies(self,image,show=False,method=1):
-
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                # Load the Haar cascade for face detection
-
+        return faces
+    
+    
+    def detectBodyLocations(self,image,show=False,method=1):
         match method:
             case 0:
-                body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
-                bodies = body_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=3, minSize=(50, 100))
+                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                bodies = self.body_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=3, minSize=(50, 100))
             case 1:
-                hog = cv2.HOGDescriptor()
-                hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-                bodies, _ = hog.detectMultiScale(image, winStride=(8, 8), padding=(8, 8), scale=1.05)
-        
-        
+                bodies, _ = self.hog.detectMultiScale(image, winStride=(8, 8), padding=(8, 8), scale=1.05)
         
         print(f"Number of bodies detected: {len(bodies)}")
-
-        # Display individual faces
+        # görselleri ver eğer show ile istenirse
         if show:
             for i, (x, y, w, h) in enumerate(bodies):
                     body = image[y:y+h, x:x+w]  
@@ -67,6 +54,12 @@ class DetectionEngine:
                     cv2.imshow(window_name, body)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+        return bodies
+
+
+    def imageDownScale(image, desiredSize=(450,450)):
+        return cv2.resize(image, desiredSize, interpolation=cv2.INTER_AREA)
+
 
 
 # Main program
@@ -78,10 +71,11 @@ if __name__ == "__main__":
 
     testEngine = DetectionEngine()
 
-    testEngine.getFaces(image, show=True)
+    testEngine.detectFaceLocations(image, show=True)
 
 
     image_path = "body.png"
-
     image = cv2.imread(image_path)
-    testEngine.getBodies(image, show=True,method=1)
+    testEngine.detectFaceLocations(image, show=True)
+
+    testEngine.detectBodyLocations(image, show=True,method=1)
